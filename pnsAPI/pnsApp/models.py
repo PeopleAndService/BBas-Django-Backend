bf078987a3eedcc335aed9c99f6931bb009b626c
황혁주 <nugulhie@gmail.com>
2021-09-29 18:02:10 +0900

    #2 Dev DataModel, ModelSerializers

--- pnsAPI/pnsApp/models.py
+++ pnsAPI/pnsApp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.postgres.fields import ArrayField
-class daDB(models.Model):  #신청 테이블
+class QueueData(models.Model):  #신청 테이블
     '''DB column'''
     busStopId               = models.CharField(max_length=50, null=False) #버스 정류장 ID
     busId                   = models.CharField(max_length=50, null=False) #버스 ID / 노선 ID 중에 하나가 될것 같음 
@@ -11,15 +9,26 @@ class daDB(models.Model):  #신청 테이블
     def __str__(self):
         return self.busStopID
 
-class Account(models.Model): #사용자 테이블
-    userId                  =models.CharField(max_length=50, primary_key=True)
-    userName                =models.CharField(max_length=15, null=True)
-    password                =models.CharField(max_length=400)
-    email                   =models.EmailField(max_length=200)
-    token                   =models.CharField(max_length=200)
-    is_activate             =models.BooleanField(default=False)
-    created_at              =models.DateTimeField(auto_now_add=True)
-    updated_at              =models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        ordering =['created_at']
+class PassengerAccount(models.Model): #사용자 테이블
+    uid                     = models.CharField(max_length=255, primary_key=True, null=False)
+    email                   = models.CharField(max_length=45, null=False)       #user_id
+    name                    = models.CharField(max_length=10, null=False)
+    pushToken               = models.CharField(max_length=255, null=True)       #로그인 후에 푸쉬 세팅을 Allow 하면 토큰을 받아오고 로그아웃하거나, 푸쉬세팅을 not Allow하면 토큰을 지운다.
+    pushSetting             = models.BooleanField(null=False, default=True)
+    emergencyPhone          = models.CharField(max_length=13, null=True)
+    verified                = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return self.pushToken
+
+class DriverAccount(models.Model): #사용자 테이블
+    uid                     = models.CharField(max_length=255, primary_key=True, null=False)
+    did                     = models.CharField(max_length=255, null=False)      #driver id
+    email                   = models.CharField(max_length=45, null=False)       #user id
+    name                    = models.CharField(max_length=10, null=False)
+    pushToken               = models.CharField(max_length=255, null=True)       #로그인 후에 푸쉬 세팅을 Allow 하면 토큰을 받아오고 로그아웃하거나, 푸쉬세팅을 not Allow하면 토큰을 지운다.
+    pushSetteing            = models.BooleanField(null=False, default=True)
+    verified                = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return self.pushToken
--- pnsAPI/pnsApp/serializers.py
+++ pnsAPI/pnsApp/serializers.py
@@ -1,13 +1,43 @@
 from rest_framework import serializers
-from .models import daDB, Account
+from .models import QueueData, DriverAccount, PassengerAccount
 
-class pnsAPIserializers(serializers.ModelSerializer):
+class pnsAPIserializer(serializers.ModelSerializer):
     class Meta:
-        model = daDB
-        fields = ['id','busStopId', 'busId', 'userId','boardingCheck'] #JSON INPUT 목록
+        model = QueueData
+        fields = ['busStopId', 'busId', 'userId', 'boardingCheck'] #JSON INPUT 목록
     
 
-class AccountSerializers(serializers.ModelSerializer):
+class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Account
-        fields = ['userId','userName','password', 'token', 'email']
+        model = PassengerAccount
+        fields = ['uid', 'email', 'name', 'pushToken', 'pushSetting', 'emergencyPhone', 'verified']
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DriverAccount
+        fields = ['uid', 'did', 'email', 'name', 'pushToken', 'pushSetting', 'verified']
+
+
+        '''
+        QUEUEDATA
+    busStopId               = models.CharField(max_length=50, null=False)
+    busId                   = models.CharField(max_length=50, null=False)
+    userId                  = models.CharField(max_length=50, null=False)
+    boardingCheck           = models.BooleanField(default=False, auto_created=True)
+        PASSENGERACCOUNT
+    uid                     = models.CharField(max_length=255, primary_key=True, null=False)
+    email                   = models.CharField(max_length=45, null=False)       
+    name                    = models.CharField(max_length=10, null=False)
+    pushToken               = models.CharField(max_length=255, null=True)       
+    pushSetting             = models.BooleanField(null=False, default=True)
+    emergencyPhone          = models.CharField(max_length=13, null=True)
+    verified                = models.BooleanField(null=False, default=False)
+        DRIVERACCOUNT
+    uid                     = models.CharField(max_length=255, primary_key=True, null=False)
+    did                     = models.CharField(max_length=255, null=False)      
+    email                   = models.CharField(max_length=45, null=False)       
+    name                    = models.CharField(max_length=10, null=False)
+    pushToken               = models.CharField(max_length=255, null=True)       
+    pushSetteing            = models.BooleanField(null=False, default=True)
+    verified                = models.BooleanField(null=False, default=False)
+        '''
