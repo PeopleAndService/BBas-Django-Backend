50f6220bf1242656a5f63535838cedd0c13279a5
황혁주 <nugulhie@gmail.com>
2021-10-01 21:41:13 +0900

    #2 Edit DataModel, ModelSerializer

--- pnsAPI/pnsApp/views.py
+++ pnsAPI/pnsApp/views.py
@@ -62,6 +62,7 @@ def pns_list(request, pk):
             pns_serializer.save()
             return JsonResponse(pns_serializer.data, status = status.HTTP_201_CREATED)
         return JsonResponse(pns_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif (request.method == 'PUT'):
         data = JSONParser().parse(request)
         serializer = pnsAPIserializers(pnsdata, data=data)
@@ -69,64 +70,9 @@ def pns_list(request, pk):
             serializer.save()
             return JsonResponse(serializer.data)
         return JsonResponse(serializer.errors, status=400)
+
     elif (request.method == 'DELETE'):
         pnsdata.delete()
         return JsonResponse(status=204)
 
-
-
-#-------------------Account management method area----------------------------
-
-@api_view(['GET','POST'])
-def account_SignUp(request):
-
-    if(request.method == 'POST'): 
-        account_data = JSONParser().parse(request)
-        account_serializer = AccountSerializers(data = account_data)
-        if (account_serializer.is_valid()):
-
-            account_serializer.save()
-            return JsonResponse(account_serializer.data, status = status.HTTP_201_CREATED)
-        return JsonResponse(account_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-        
-    elif(request.method == 'GET'):
-        accounts = Account.objects.all()
-        userId = request.GET.get('userId',None)
-        if userId is not None:
-            accounts = accounts.filter(userId__icontains=userId)
-        account_serializer = AccountSerializers(accounts, many=True)
-        return JsonResponse(account_serializer.data, safe=False)
-
-@api_view(['POST'])
-def account_SignIn(request):
-    if(request.method == 'POST'): 
-        login_data = JSONParser().parse(request)
-        if login_data is not None:
-            inputUserId = login_data['userId']
-            print(inputUserId)
-            inputPassword = login_data['password']
-            login = Account.objects.all()
-            login = list(login.filter(userId__icontains=inputUserId))
-            if(login[0].password == inputPassword): 
-                return JsonResponse({"passed":"True"}, safe=False, status=status.HTTP_200_OK)
-            else:
-                return JsonResponse({"passed":"False"}, safe=False, status=status.HTTP_401_UNAUTHORIZED)
-        
-
-def account_Authentication(email):
-    token = email
-    return token
-
-
-class ProjectCreate(generics.CreateAPIView):
-    quertset = daDB.objects.all(),
-    serializer_class = pnsAPIserializers
-
-class ProjectList(generics.ListAPIView):
-    queryset = daDB.objects.all()
-    serializer_class = pnsAPIserializers
-
-class SignUp(generics.CreateAPIView):
-    quertset = Account.objects.all()
-    serializer_class = AccountSerializers
 
